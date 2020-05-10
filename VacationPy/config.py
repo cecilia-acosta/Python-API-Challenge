dcf13d9c0d3525a5de591cb64d2ab1bb65fb40a5
Cecilia Acosta Garcia <cecilia.acosta.garcia@gmail.com>
2020-05-10 01:54:33 -0500

    Python API Homework upload

---
+++ VacationPy/config.py
@@ -0,0 +1,5 @@
+# Add your API key
+api_key_NYTimes = "DMw4SAZ7jeZusV4djgGCrkx8tF0A3KkM"
+api_key_OpenWeather = "e7aaaedb2816c2d012380e01b34ccb76"
+api_key_Census = "78029eed8bacec60c5f1112dd71f78c371866b6c"
+gkey = "AIzaSyAZ6cFT_wZHSIcAfRfST7wOaGLy8HFFsOY"
